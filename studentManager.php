31d07b5206692cb3a5464c40db844d07378587d0
Shanta-Islam-pro <sishantaislam49@gmail.com>
2026-01-20 00:41:40 +0600

    Fix formatting issues in StudentManager class

--- studentManager.php
+++ studentManager.php
@@ -8,26 +8,26 @@ class StudentManager
         $this->file = $file;
     }
 
-    // JSON ফাইল থেকে সব student পড়া
+    
     private function readData()
     {
         $json = file_get_contents($this->file);
         return json_decode($json, true);
     }
 
-    // JSON ফাইলে সব student লেখা
+    
     private function writeData($data)
     {
         file_put_contents($this->file, json_encode($data, JSON_PRETTY_PRINT));
     }
 
-    // সব student দেখানো
+ 
     public function getAllStudents()
     {
         return $this->readData();
     }
 
-    // ID দিয়ে student খোঁজা
+    
     public function getStudentById($id)
     {
         $students = $this->readData();
@@ -41,19 +41,19 @@ class StudentManager
         return null;
     }
 
-    // নতুন student যোগ করা
+   
     public function create($data)
     {
         $students = $this->readData();
 
-        // Duplicate ID চেক
+       
         foreach ($students as $student) {
             if ($student['id'] == $data['id']) {
                 return false;
             }
         }
 
-        // Email validation
+     
         if (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
             return false;
         }
@@ -63,7 +63,7 @@ class StudentManager
         return true;
     }
 
-    // student আপডেট করা
+    
     public function update($id, $data)
     {
         $students = $this->readData();
@@ -79,7 +79,7 @@ class StudentManager
         return false;
     }
 
-    // student ডিলিট করা
+  
     public function delete($id)
     {
         $students = $this->readData();
@@ -95,4 +95,5 @@ class StudentManager
         return false;
     }
     
+
 }
